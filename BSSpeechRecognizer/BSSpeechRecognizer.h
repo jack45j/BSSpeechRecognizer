8783c165050616b4a22bc090d0b57aec7d395588
Benson Lin <jack45j@gmail.com>
2023-02-20 14:22:16 +0800

    Initial Commit

---
+++ BSSpeechRecognizer/BSSpeechRecognizer.h
@@ -0,0 +1,18 @@
+//
+//  BSSpeechRecognizer.h
+//  BSSpeechRecognizer
+//
+//  Created by 林翌埕-20001107 on 2023/2/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BSSpeechRecognizer.
+FOUNDATION_EXPORT double BSSpeechRecognizerVersionNumber;
+
+//! Project version string for BSSpeechRecognizer.
+FOUNDATION_EXPORT const unsigned char BSSpeechRecognizerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BSSpeechRecognizer/PublicHeader.h>
+
+
